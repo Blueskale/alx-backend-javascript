29bf77b272795d6f6dfea8288bb5875dfcf26a4c
Blueskale <princewillmmadu@gmail.com>
2023-10-08 10:03:37 -0700

    Basics

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,11 @@
+function getCurrentYear() {
+  const year = new Date();
+  return year.getFullYear();
+}
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
